5a070ce3531bd54959bf02f7a15b8c714e022e8c
song <songnumber1@naver.com>
2023-09-15 22:14:05 +0900

    Configuration과 properties파일로 동적 빈 생성 시 ConditionalOnProperty으로 빈 추가 설정까지 수정

--- src/main/java/com/example/mybatis/demo/mybatisdemo/bean/BeanManager.java
+++ src/main/java/com/example/mybatis/demo/mybatisdemo/bean/BeanManager.java
@@ -1,6 +1,7 @@
 package com.example.mybatis.demo.mybatisdemo.bean;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class BeanManager {
     String dynamicTypeBean;
 
     @Bean
+    @ConditionalOnProperty(value = "bean.config.dynamicbean.enable", havingValue = "true", matchIfMissing = false)
     public DynamicBeanService GetDynamicBean()
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         DynamicBeanService obj = (DynamicBeanService) Class.forName(dynamicBean).newInstance();
@@ -21,8 +23,11 @@ public class BeanManager {
     }
     
     @Bean
-    public DynamicBeanTypeService<DynamicBeanTypeGenericVo> GetDynamicTypeBean() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-        DynamicBeanTypeService<DynamicBeanTypeGenericVo> obj = (DynamicBeanTypeService<DynamicBeanTypeGenericVo>)Class.forName(dynamicTypeBean).newInstance();
+    @ConditionalOnProperty(value = "bean.config.dynamictypebean.enable", havingValue = "true", matchIfMissing = false)
+    public DynamicBeanTypeService<DynamicBeanTypeGenericVo> GetDynamicTypeBean()
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        DynamicBeanTypeService<DynamicBeanTypeGenericVo> obj = (DynamicBeanTypeService<DynamicBeanTypeGenericVo>) Class
+                .forName(dynamicTypeBean).newInstance();
 
         return obj;
     }
