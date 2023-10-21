531d1beff9a74d812a4f4a2e2aa92eff5796c381
song <songnumber1@naver.com>
2023-10-21 15:28:32 +0900

    동적 서비스 생성 시 테스트에서 @Autowired 사용하여 테스트

---
+++ src/test/java/com/example/mybatis/demo/mybatisdemo/RuntimeMockitoTest.java
@@ -0,0 +1,74 @@
+
+package com.example.mybatis.demo.mybatisdemo;
+
+import static org.mockito.BDDMockito.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import com.example.mybatis.demo.mybatisdemo.runtime.RuntimeService;
+
+@SpringBootTest
+//@ExtendWith(MockitoExtension.class)
+@Import({RuntimeTestConfig.class})
+public class RuntimeMockitoTest {
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @Test
+    @DisplayName("MockitoRuntimeServiceImpl1CallMethod")
+    @SuppressWarnings("unchecked")
+    void MockitoRuntimeServiceImpl1CallMethod() {
+        String result = this.runtimeService.CallMethod("1234");
+
+        assertTrue(true);
+        // var returnData = "RumtimeServiceImpl1.CallMethod()";
+
+        // // given
+        // // CallMethod를 호출 시 RumtimeServiceImpl1.CallMethod()으로 리턴값을 설정
+        // when(runtimeService.CallMethod("")).thenReturn(returnData);
+
+        // // when
+        // // stub에서 CallMtthod("1")로 호출 시에는 result값과 동일한 값이 출력되지 않으니 파라미터까지 모두 동일해아한다.
+        // String result = this.runtimeService.CallMethod("");
+
+        // // then
+        // verify(runtimeService, times(1)).CallMethod("");
+
+        // // then
+        // assertTrue(returnData == result);
+    }
+
+    @Test
+    @DisplayName("BDDMockitoRuntimeServiceImpl1CallMethod")
+    @SuppressWarnings("unchecked")
+    void BDDMockitoRuntimeServiceImpl1CallMethod() {
+        var returnData = "RumtimeServiceImpl1.CallMethod()";
+        
+        // given
+        given(runtimeService.CallMethod("")).willReturn(returnData);
+
+        // when
+        String result = runtimeService.CallMethod("");
+
+        // then
+        verify(runtimeService, times(1)).CallMethod("");
+        // then
+        assertTrue(returnData == result);
+	}
+}
--- src/test/java/com/example/mybatis/demo/mybatisdemo/RuntimeTest.java
+++ src/test/java/com/example/mybatis/demo/mybatisdemo/RuntimeTest.java
@@ -1,25 +1,21 @@
 
 package com.example.mybatis.demo.mybatisdemo;
 
-import static org.mockito.BDDMockito.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import com.example.mybatis.demo.mybatisdemo.runtime.RuntimeService;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Import({RuntimeTestConfig.class})
 public class RuntimeTest {
-    @Mock
+    @Autowired
     @SuppressWarnings("rawtypes")
     private RuntimeService runtimeService;
 
@@ -31,38 +27,10 @@ public class RuntimeTest {
     @DisplayName("MockitoRuntimeServiceImpl1CallMethod")
     @SuppressWarnings("unchecked")
     void MockitoRuntimeServiceImpl1CallMethod() {
-        var returnData = "RumtimeServiceImpl1.CallMethod()";
+        String result = "RumtimeServiceImpl1.CallMethod() : 1234";
 
-        // given
-        // CallMethod를 호출 시 RumtimeServiceImpl1.CallMethod()으로 리턴값을 설정
-        when(runtimeService.CallMethod("")).thenReturn(returnData);
+        String returnData = this.runtimeService.CallMethod("1234");
 
-        // when
-        // stub에서 CallMtthod("1")로 호출 시에는 result값과 동일한 값이 출력되지 않으니 파라미터까지 모두 동일해아한다.
-        String result = this.runtimeService.CallMethod("");
-
-        // then
-        verify(runtimeService, times(1)).CallMethod("");
-
-        // then
-        assertTrue(returnData == result);
+        assertTrue(returnData.equals(result));
     }
-
-    @Test
-    @DisplayName("BDDMockitoRuntimeServiceImpl1CallMethod")
-    @SuppressWarnings("unchecked")
-    void BDDMockitoRuntimeServiceImpl1CallMethod() {
-        var returnData = "RumtimeServiceImpl1.CallMethod()";
-        
-        // given
-        given(runtimeService.CallMethod("")).willReturn(returnData);
-
-        // when
-        String result = runtimeService.CallMethod("");
-
-        // then
-        verify(runtimeService, times(1)).CallMethod("");
-        // then
-        assertTrue(returnData == result);
-	}
 }
